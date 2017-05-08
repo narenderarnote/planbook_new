258f5d19c8109ed139e85619ba2e48b1b2d87c53
Gurinder Sandhu <gurinder@techsparksit.com>
2017-05-08 11:40:14 +0530

    add class lesson migration

---
+++ database/migrations/2017_05_08_054502_create_class_lessons_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_lessons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('class_id')->unsigned();
+            $table->integer('user_id')->nullable();
+            $table->date('lesson_date')->nullable();
+            $table->time('lesson_start_time')->nullable();
+            $table->time('lesson_end_time')->nullable();
+            $table->string('unit')->nullable();
+            $table->string('lesson_title')->nullable();
+            $table->text('lesson_text')->nullable();
+            $table->text('homework')->nullable();
+            $table->text('notes')->nullable();
+            $table->text('standards')->nullable();
+            $table->text('attachments')->nullable();
+            $table->integer('lock_lesson_to_date')->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('class_lessonsd');
+
+    }
+}
