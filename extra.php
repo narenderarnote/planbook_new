677d09b5a042719811bb213f8161183cfd0c1324
Gurinder Sandhu <gurinder@techsparksit.com>
2017-05-08 18:51:09 +0530

    add extra.php server details~

---
+++ extra.php
@@ -0,0 +1,18 @@
+ssh pass AcCo.8hE~hfN
+Domain: techsparksit.com
+cPanel Username:  techsp12
+Password: sparkstech@12345
+Server: ecbiz219
+ip 144.208.78.49
+Hostname: ecbiz219.inmotionhosting.com
+Temp URL: http://ecbiz219.inmotionhosting.com/~techsp12
+Temp Control Panel: http://ecbiz219.inmotionhosting.com/cpanel
+Temp Webmail:http://ecbiz219.inmotionhosting.com/webmail
+Shared Secure Server:https://secure219.inmotionhosting.com/~techsp12
+Control Panel: http://www.techsparksit.com/cpanel
+Webmail: http://www.techsparksit.com/webmail
+Nameserver 1: ns.inmotionhosting.com
+Nameserver 2: ns2.inmotionhosting.com
+Nameserver 1 Ip: 74.124.210.242
+Nameserver 2 Ip: 70.39.150.2
+DNS Authority Key: akpDekQyRX…RuZGxqbEs5
