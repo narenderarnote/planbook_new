4e8bc1b2e1bda22ae5d69b539934fa8a73ce7a79
Gurinder Sandhu <gurinder@techsparksit.com>
2017-05-10 18:06:57 +0530

    add files routes

--- extra/guri.php
+++ extra/guri.php
@@ -58,3 +58,31 @@ Nameserver 2: ns2.inmotionhosting.com
 Nameserver 1 Ip: 74.124.210.242
 Nameserver 2 Ip: 70.39.150.2
 DNS Authority Key: akpDekQyRX…RuZGxqbEs5
+
+
+
+ Your My UPS User ID: PLopez2106
+
+Password: Ups12345
+
+Your UPS Account Number: 22W604
+
+Patrick Lopez
+
+patricklopez210@yahoo.com
+
+our Control Panel: https://gator4255.hostgator.com:2083
+    Username:           dynamiksigns
+    Domain:             i9signs.com
+    Password:           EMi9nkz#YWeWmLW$lJ
+    1st Nameserver:     ns8509.hostgator.com
+    2nd Nameserver:     ns8510.hostgator.com
+    Server IP:          108.167.189.81
+
+pass EMi9nkz#YWeWmLW$lJ
+
+
+paypal cred:
+
+jitendraqwq@techsparksit.com
+jitendra@@07
